5c239165dd0bfc6718740dcc69dd5e8cf1418c6c
ruler1216 <accelerator1216wk@gmail.com>
2023-02-04 23:21:44 +0900

    commit message

---
+++ ruby_ex/ex1707.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+# 配列にデータを作成
+fruits = ["apple", "banana", "cherry", "fig", "grape"]
+
+# ファイル名に変数を割り当てる
+fname = "sample7.txt"
+
+# "sample7.txt"を書き込みモードで新規に作成
+open(fname, "w"){|file|
+    fruits.each{|fruit|
+        file.puts fruit
+        }
+    }
+
+# "sample7.txt"を読み込みモードでオープンする
+open(fname, "r"){|file|
+    #ファイルの行を1行ずつ取り出して、lineに読み込む
+    file.each{|line|
+        #lineを表示する
+        print line
+    }
+}
+# "sample7.txt"を削除する
+File.delete(fname)
+
+# "sample7.txt"を読み込みモードでオープンする
+# ファイルが削除されているのでエラーが発生するはず
+open("fname", "r")
