11841631ba73cfbe343be7568ab6491d3cfb3b2a
ruler1216 <accelerator1216wk@gmail.com>
2023-03-04 05:15:06 +0900

    commit message

--- 1day_rails/health_observation/config/routes.rb
+++ 1day_rails/health_observation/config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  post '/healths/submit', to: 'healths#submit'
+  get '/healths', to: 'healths#submit'
 end
