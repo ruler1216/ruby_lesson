8da3ff7503121fccfbc6af5bd16cba8044817b5a
ruler1216 <accelerator1216wk@gmail.com>
2023-03-05 01:17:12 +0900

    commit message

--- 1day_rails/health_observation/app/controllers/healths_controller.rb
+++ 1day_rails/health_observation/app/controllers/healths_controller.rb
@@ -1,14 +1,19 @@
 class HealthsController < ApplicationController
+    def index
+        @health=Health.new
+    end
     # 提出用のアクション
-    def submit
+    def create
         # Healthモデルのインスタンス@healthの生成
-        @health=Health.new#(healths_paramS)
+        @health=Health.new(healths_params)
         # ビューファイルで入力された@healthの情報をデータベースに保存
-        #@health.save
+        @health.save!
+        redirect_to(task_url, notice: "送信が完了しました。")
     end
 
     # newメソッドに渡す情報を受け取るアクション
-    #def healths_params
-        #params.require(:health).permit(:year, :school_class, :class_number, :name)
-    #end
+    private
+    def healths_params
+        params.require(:healths).permit(:year,:school_class,:class_number,:name,:commute,:temperature,:etc)
+    end
 end
--- 1day_rails/health_observation/config/routes.rb
+++ 1day_rails/health_observation/config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/healths', to: 'healths#submit'
+  get 'healths/index'
+  post 'healths/create', to: 'users#create'
+  
+  resources :posts
+  get 'users/index'
 end
