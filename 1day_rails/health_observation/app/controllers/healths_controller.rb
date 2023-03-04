f6bfddfdac5e99fab9c83c9282685f3a2bb12c20
ruler1216 <accelerator1216wk@gmail.com>
2023-03-05 03:58:50 +0900

    commit message

--- 1day_rails/health_observation/app/controllers/healths_controller.rb
+++ 1day_rails/health_observation/app/controllers/healths_controller.rb
@@ -1,8 +1,10 @@
 class HealthsController < ApplicationController
+  before_action :set_q, only: [:show, :search]
   def index
   end
 
   def show
+    @health=Health.all
   end
 
   def new
@@ -15,9 +17,17 @@ class HealthsController < ApplicationController
     redirect_to(healths_url, notice: "送信を完了しました。")
   end
 
+  def search
+    @results=@q.result
+  end
+
   private
 
   def health_params
     params.require(:health).permit(:year, :school_class, :class_number, :name, :commute, :temperature, :etc)
+  end
+
+  def set_q
+    @q=Health.ransack(params[:q])
   end
 end
